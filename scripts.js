ba9c0175da4127ea34c40e1fbe9b21ba08f5c868
Shashank <111672505+Shashank-Chowdary@users.noreply.github.com>
2024-06-19 10:22:00 +0530

    scripts.js

--- scripts.js
+++ scripts.js
@@ -26,7 +26,7 @@ function showSection(sectionId) {
     }
 }
 
-function downloadPDF() {
+function downloadResume() {
     const link = document.createElement('a');
     link.href = 'Pentala Shashank Resume.pdf'; // Replace with the correct path to your PDF file
     link.download = 'Pentala Shashank Resume.pdf';
@@ -37,6 +37,16 @@ function downloadPDF() {
     alert('Thanks for downloading my resume!');
 }
 
+function downloadCV() {
+    const link = document.createElement('a');
+    link.href = 'Shashank Pentala CV.pdf'; // Replace with the correct path to your PDF file
+    link.download = 'Shashank Pentala CV.pdf';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+
+    alert('Thanks for downloading my CV!');
+}
 function restartVideo() {
     const video = document.getElementById('bg-video');
     video.currentTime = 0;
